0ff3c8bf73e2d399a3588e5df1351521674e1164
Dulon Mahadi <dulonmolla88@gmail.com>
2023-05-25 20:28:33 +0600

    recipes person data is eready

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const recipes_person = require('./public/recipesPerson.json');
+// const recipes = require('./public/recipes.json');
+
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
@@ -12,10 +15,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 //all methods are here
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/recipes_person', (req, res) => {
+  res.send(recipes_person);
+});
+
+// app.get('/recipes_person/recipes', (req, res) => {
+//   res.send(recipes);
+// });
 
 app.listen(port, () => {
   console.log(`Server Running on Port: http://localhost:${port}`);
--- readMe.md
+++ readMe.md
@@ -1,6 +1,11 @@
 # steps after clone:
 
--set the PORT = 3000 in .env file.
--npm install
--npm start
--lets go to port 3000
+** set the PORT = 3000 in .env file. **
+
+** npm install **
+
+** npm start **
+
+** lets go to port 3000 **
+
+## ready the recipes indivisual data.and call it /recipes_person
